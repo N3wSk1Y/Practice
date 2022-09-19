7dd930604a78fbfcb79c8f9f373dcdd3ee000b7b
N3wSk1Y <miner.fedenko@gmail.com>
2022-09-19 10:31:14 +0300

    Practice 3

---
+++ Практика 3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace practice3;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите значения параметров a, b, c через запятую");
+        string[] parameters = Console.ReadLine().Replace(" ", "").Split(',');
+        double a = double.Parse(parameters[0]);
+        double b = double.Parse(parameters[1]);
+        double c = double.Parse(parameters[2]);
+        double result = Math.Pow(2, a) * Math.Pow(a + b + c, (double)1/3) + (Math.Pow(c, a + b) / Math.Sin(a/b)) - 0.2;
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
