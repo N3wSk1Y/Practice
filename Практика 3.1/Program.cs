9548039d53d5165adbf513dec32454792572c270
dima <miner.fedenko@gmail.com>
2022-09-20 16:26:06 +0300

    Practice 3.1

---
+++ Практика 3.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace practice3;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        int input = int.Parse(Console.ReadLine());
+        Console.WriteLine(input);
+        Console.ReadKey();
+    }
+}
