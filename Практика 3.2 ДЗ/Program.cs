c730cb2bd8e053a6b25f42d96e250161b231e5fd
N3wSk1Y <miner.fedenko@gmail.com>
2022-09-21 20:04:43 +0300

    Practice 3.2 (Homework)

---
+++ Практика 3.2 ДЗ/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+namespace practice3;
+
+public static class Program
+{
+    private static void Task1()
+    {
+        int range = int.Parse(Console.ReadLine());
+        if (range >= 30)
+        {
+            Console.WriteLine("Перелет!");
+        }
+        else if (range >= 28)
+        {
+            Console.WriteLine("Попал!");
+        }
+        else if (range >= 0)
+        {
+            Console.WriteLine("Недолет!");
+        }
+
+        Console.ReadKey();
+    }
+
+    private static void Task2()
+    {
+        int sybmol = int.Parse(Console.ReadLine());
+        string result = 9 - sybmol >= 0 ? "Да" : "Нет";
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+    
+    private static void Task3()
+    {
+        string[] parameters = Console.ReadLine().Replace(" ", "").Split(',');
+        double a = double.Parse(parameters[0]);
+        double b = double.Parse(parameters[1]);
+        double c = double.Parse(parameters[2]);
+        double D = Math.Pow(b, 2) - 4 * a * c;;
+
+        Console.Write($"Уравнение с коэффициентами {a}, {b}, {c} ");
+        if (D >= 0)
+        {
+            double x1 = (-b + Math.Sqrt(D)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(D)) / (2 * a);
+            if (x1 == x2)
+            {
+                Console.Write($"имеет один корень: {x1}");
+            }
+            else
+            {
+                Console.Write($"имеет два корня: {x1} и {x2}");
+            }
+        }
+        else
+        {
+            Console.Write("не имеет действительных корней.");
+        }
+        
+        Console.ReadKey();
+    }
+
+    private static void Task4()
+    {
+        string[] parameters = Console.ReadLine().Replace(" ", "").Split(',');
+        int a = int.Parse(parameters[0]);
+        int b = int.Parse(parameters[1]);
+        int c = int.Parse(parameters[2]);
+        
+    }
+    private static void Task5()
+    {
+        char sybmol = char.Parse(Console.ReadLine().ToLower());
+        // string vowels = "aeiou";
+        // string result = vowels.IndexOf(sybmol.ToString()) >= 0 ? "Гласная" : "Согласная";
+        // Console.WriteLine(result);
+
+        switch (sybmol)
+        {
+            case 'a': Console.WriteLine("Гласная"); break;
+            case 'e': Console.WriteLine("Гласная"); break;
+            case 'i': Console.WriteLine("Гласная"); break;
+            case 'o': Console.WriteLine("Гласная"); break;
+            case 'u': Console.WriteLine("Гласная"); break;
+            default: Console.WriteLine("Согласная"); break;
+        }
+    }
+
+    public static void Main()
+    {
+        Task4();
+    }
+}
