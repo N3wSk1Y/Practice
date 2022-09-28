51814ef855763af2877885465b22d6d102bb7d7d
N3wSk1Y <miner.fedenko@gmail.com>
2022-09-28 20:20:02 +0300

    UValue returning

--- Практика 5/Program.cs
+++ Практика 5/Program.cs
@@ -12,8 +12,16 @@ class Program
         return false;
     }
 
+    static double UValue(float x, float y)
+    {
+        if (includedInArea(x, y))
+            return Math.Abs(1 - x * y);
+        else
+            return Math.Pow(x, 2) - 5;
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine(includedInArea(0, -4.5f));
+        Console.WriteLine(UValue(0, -4.5f));
     }
 }
