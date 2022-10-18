1d8dec06884c84e73677e005f6a6f6fb3c348bcd
dima <miner.fedenko@gmail.com>
2022-10-18 15:02:38 +0300

    Practice 7

---
+++ Практика 7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace practice7;
+
+internal class Program
+{
+    private static void Task1()
+    {
+        for (int i = 2; i <= 9; i++)
+        {
+            Console.WriteLine();
+            for (int y = 2; y <= 5; y++)
+            {
+                Console.Write($"{y} * {i} = {i*y}\t");
+            }
+        }
+
+        Console.WriteLine();
+        for (int i = 2; i <= 9; i++)
+        {
+            Console.WriteLine();
+            for (int y = 6; y <= 9; y++)
+            {
+                Console.Write($"{y} * {i} = {i*y}\t");
+            }
+        }
+    }
+    
+    public static void Main(string[] args)
+    {
+        Task1();
+    }
+}
