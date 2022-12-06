4cd92f54ec0e4c894eaf8512f91601fd7100a54f
N3wSk1Y <miner.fedenko@gmail.com>
2022-12-06 20:31:36 +0300

    Фоновая 2.2

--- Фоновая 2.2/Program.cs
+++ Фоновая 2.2/Program.cs
@@ -3,40 +3,59 @@ namespace phone22;
 
 internal class Program
 {
-    private static void CommandsHandler(int command)
+    private static long number = 0;
+    private static void HandleCommand(string command, int argument)
     {
         switch (command)
         {
-            case 1: // Занести
+            case "/write": // Занести
+                
+                break;
+            case "/read": // Прочитать
 
                 break;
-            case 2: // Прочитать
-
-                break;
-            case 3: // Очистить
+            case "/clear": // Очистить
 
                 break;
             default:
-                AwaitCommand("Неверная команда.");
+                Console.WriteLine("\nНеверная команда.");
+                AwaitCommand();
                 break;
         }
+        Console.WriteLine(BitsRepresentation(ref number));
     }
-    private static void AwaitCommand(string data)
+    private static void ReadSector(long number, int sector)
     {
-        Console.WriteLine(data);
-        Console.WriteLine("Введите команду:\n1 - занести\n2 - прочитать\n3 - очистить");
-        while (true)
+        int m = 15 << sector;
+    }
+    private static void AwaitCommand()
+    {
+        Console.WriteLine("Введите команду:\n/write [число (1-15)] - занести\n/read [ячейка (1-16)] - прочитать\n/clear [ячейка (1-16)] - очистить");
+        string input = Console.ReadLine();
+        string[] words = input.Split(' ');
+        if (words.Length == 2)
         {
-            string input = Console.ReadLine();
-            if (Int32.TryParse(input, out int command))
-                CommandsHandler(command);
-            else
-                Console.WriteLine();
+            if (Int32.TryParse(words[1], out int argument))
+            {
+                HandleCommand(words[0], argument);
+            }
+            return;
         }
+        Console.WriteLine("\nНеверная команда.");
+        AwaitCommand();
+    }
+    private static string BitsRepresentation(ref long number)
+    {
+        string binaryRepresentation = Convert.ToString(number, 2);
+        string result = "";
+        for (int i = 64; i > binaryRepresentation.Length; i--)
+            result += "0";
+        
+        return result += binaryRepresentation;
     }
     public static void Main(string[] args)
     {
-        long number = 0;
-        AwaitCommand("Архиватор.");
+        AwaitCommand();
+        // Console.WriteLine(BitsRepresentation(ref number));
     }
 }
