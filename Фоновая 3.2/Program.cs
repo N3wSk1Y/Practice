e7e677f98ffa4d3484839dfa9abb4b600926c338
n3wsk1y <miner.fedenko@gmail.com>
2022-12-20 16:28:46 +0300

    Фоновая 3.2

--- Фоновая 3.2/Program.cs
+++ Фоновая 3.2/Program.cs
@@ -3,8 +3,19 @@ namespace phone32;
 
 internal class Program
 {
-    private static int F()
+    private static int F(int n)
     {
-        
+        if (n > 10)
+            return n * n + 5 * n + 4;
+        else if (n % 2 == 0)
+            return F(n + 1) + 3 * F(n + 4);
+        else
+            return 2 * F(n + 2) + F(n + 5);
+    }
+
+    public static void Main(string[] args)
+    {
+        int t1, t2, t3;
+        Console.WriteLine(F(5));
     }
 }
