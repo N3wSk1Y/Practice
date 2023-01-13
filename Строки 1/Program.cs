d028bbe366f56592100bde5d28b985bbe5fd0563
n3wsk1y <miner.fedenko@gmail.com>
2023-01-13 11:42:05 +0300

    Refactoring

---
+++ Строки 1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+namespace stringpractice1;
+
+internal class Program
+{
+    private static int Task1(string path)
+    {
+        int maxNumber = 0, localMax = 0;
+        var fs = new StreamReader(path);
+        string s = fs.ReadLine();
+        char[] symbols = s.ToCharArray();
+        char prepreviousSymbol = 'A', previousSymbol = 'A';
+        for (int i = 0; i < symbols.Length; i++)
+        {
+            if (prepreviousSymbol != symbols[i] && prepreviousSymbol != 'Z' && symbols[i] != 'Z')
+            {
+                localMax++;
+            }
+            else
+            {
+                if (maxNumber < localMax)
+                    maxNumber = localMax;
+                localMax = 0;
+            }
+            prepreviousSymbol = previousSymbol;
+            previousSymbol = symbols[i];
+        }
+
+        return maxNumber;
+    }
+    
+    private static int Task2(string path)
+    {
+        int maxNumber = 0, localMax = 0;
+        var fs = new StreamReader(path);
+        string s = fs.ReadLine();
+        char[] symbols = s.ToCharArray();
+        char prepreviousSymbol = 'A', previousSymbol = 'A';
+        for (int i = 0; i < symbols.Length; i++)
+        {
+            if (prepreviousSymbol != symbols[i] && prepreviousSymbol != 'Z' && symbols[i] != 'Z')
+            {
+                localMax++;
+            }
+            else
+            {
+                if (maxNumber < localMax)
+                    maxNumber = localMax;
+                localMax = 0;
+            }
+            prepreviousSymbol = previousSymbol;
+            previousSymbol = symbols[i];
+        }
+
+        return maxNumber;
+    }
+    
+    public static void Main()
+    {
+        // Вариант 2
+        string path1 = "Task-1.txt";
+        int res1 = Task1(path1);
+        Console.WriteLine(res1);
+    }
+}
---
+++ Фоновая 3.3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace phone33;
+
+internal class Program
+{
+    private static void FindPoint()
+    {
+        
+    }
+    
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        FindPoint();
+    }
+}
