aeabc229dfcca3aed04970b0bd47fd944171e45a
n3wsk1y <miner.fedenko@gmail.com>
2023-01-17 16:00:57 +0300

    Refactoring

--- Фоновая 3.3/Program.cs
+++ Фоновая 3.3/Program.cs
@@ -3,14 +3,44 @@ namespace phone33;
 
 internal class Program
 {
-    private static void FindPoint()
+    private static int FindPoint(string s)
     {
-        
+        string localS, substring; 
+        bool isLocalS = true;
+        for (int j = 1; j <= s.Length; j++)
+        {
+            isLocalS = true;
+            if (s.Length % j != 0)
+            {
+                continue;
+            }
+            substring = s.Substring(0, j);
+            for (int i = 1; i < s.Length / j; i++)
+            {
+                localS = s.Substring(i * j, j);
+                if (localS != substring)
+                {
+                    isLocalS = false; 
+                    break;
+                }
+            }
+
+            if (isLocalS)
+            {
+                return j;
+            }
+        }
+        return 0;
     }
-    
+
+
     public static void Main()
     {
-        string s = Console.ReadLine();
-        FindPoint();
+        while (true)
+        {
+            Console.WriteLine("Введите строку");
+            int r = FindPoint(Console.ReadLine());
+            Console.WriteLine(r);
+        }
     }
 }
