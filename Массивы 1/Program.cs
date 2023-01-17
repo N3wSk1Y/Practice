f882030a7ab7c2a2e8640c6b5d1400bc125836df
n3wsk1y <miner.fedenko@gmail.com>
2023-01-17 16:01:17 +0300

    Refactoring

---
+++ Массивы 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace arrays1;
+
+internal class Program
+{
+
+    private static int[] InputIntArray(int arrayLength)
+    {
+        int[] array = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++)
+        {
+            Console.WriteLine($"Введите {i + 1} элемент массива");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        return array;
+    }
+
+    private static void PrintArray(int[] array)
+    {
+        foreach (int xm in array)
+        {
+            Console.Write(xm);
+        }
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Введите длину массива");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] array = InputIntArray(n);
+        
+        Console.WriteLine("\nВывод элементов массива:");
+        PrintArray(array);
+    }
+}
